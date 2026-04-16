e94eb4825e2fd786160596f32b605229123106fb
jwilliams140 <jwilliams140@csub.edu>
2026-04-15 17:36:18 -0700

    Stocks useState updated to include data

--- App.js
+++ App.js
@@ -4,6 +4,12 @@ import { Button, ScrollView } from "react-native";
 export default function App() {
   const [screen, setScreen] = useState("stocks");
 
+  const [stocks, setStocks] = useState([
+    { name: "AAPL", value: 100 },
+    { name: "GOOG", value: 120 },
+    { name: "MSFT", value: 80 }
+  ]);
+
   return (
     <ScrollView style={{ padding: 20 }}>
       <Button title="Stocks" onPress={() => setScreen("stocks")} />
